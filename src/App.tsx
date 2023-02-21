ab294f616231d705d2c7f794101ac3166428539c
Kari <kari@aijinet.com>
2023-02-21 11:42:44 +0900

    styled-components 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components"
+
+const Wrapper = styled.div``
+const animation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    transform: rotate(360deg);
+    border-radius: 50px;
+  }
+  100% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`
+
+const Emoji = styled.span`
+  font-size: 36px;
+`
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: teal;
+  animation: ${animation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${Emoji} {
+    &:hover {
+      font-size: 40px;
+    }
+  }
+`
+
+const Input = styled.input.attrs({ required: true, maxLength: 10 })`
+  background-color: teal;
+`
+
+const Btn = styled.button`
+  background-color: yellow;
+  color: red;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <Box>
+        <Emoji>😆</Emoji>
+      </Box>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Btn>1231231232</Btn>
+      <Btn as="a">sadlflsdkjfs</Btn>
+    </Wrapper>
+  )
 }
 
-export default App;
+export default App
